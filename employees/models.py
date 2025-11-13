68bbc16127cbcb73c02188a6f4b0c6b7aee1919a
s0560996 <ododmengapa@gmail.com>
2025-11-13 12:49:14 +0100

    first commit

---
+++ employees/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.role}"
