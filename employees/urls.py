0cead5afb0c9246880b21e23b0f92362b2e5b5a1
s0560996 <ododmengapa@gmail.com>
2025-11-23 18:32:33 +0100

    CRUD Views & REST

--- employees/urls.py
+++ employees/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework import routers
+from .views_api import EmployeeViewSet
+
+router = routers.DefaultRouter()
+router.register(r'api/employees', EmployeeViewSet)
 
 urlpatterns=[
     path('upload/', views.upload_csv, name='upload_csv'),
@@ -8,4 +13,4 @@ urlpatterns=[
     path('employee/new/', views.employee_create, name='employee_create'),
     path('employee/<int:pk>/edit/', views.employee_edit, name='employee_edit'),
     path('employee/<int:pk>/delete/', views.employee_delete, name='employee_delete'),
-]
+] + router.urls
